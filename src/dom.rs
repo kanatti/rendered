c5934915dc56e3b0e23e570dad0be6b6e78de756
kanatti <balukanattil@gmail.com>
2022-05-20 07:25:12 +0530

    Add pretty print

--- src/dom.rs
+++ src/dom.rs
@@ -1,32 +1,32 @@
 use std::collections::HashMap;
 
-struct Node {
-    children: Vec<Node>,
-    node_type: NodeType,
+pub struct Node<'a> {
+    children: Vec<Node<'a>>,
+    node_type: NodeType<'a>,
 }
 
-enum NodeType {
-    Text(String),
-    Element(ElementData),
-    Comment(String),
+pub enum NodeType<'a> {
+    Text(&'a str),
+    Element(ElementData<'a>),
+    Comment(&'a str),
 }
 
-struct ElementData {
-    tag_name: String,
-    attributes: AttrMap,
+pub struct ElementData<'a> {
+    tag_name: &'a str,
+    attributes: AttrMap<'a>,
 }
 
-type AttrMap = HashMap<String, String>;
+pub type AttrMap<'a> = HashMap<&'a str, &'a str>;
 
-impl Node {
-    fn text(data: String) -> Self {
+impl<'a> Node<'a> {
+    pub fn text(data: &'a str) -> Self {
         Node {
             children: Vec::new(),
             node_type: NodeType::Text(data),
         }
     }
 
-    fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Self {
+    pub fn elem(name: &'a str, attrs: AttrMap<'a>, children: Vec<Node<'a>>) -> Self {
         Node {
             children,
             node_type: NodeType::Element(ElementData {
@@ -36,10 +36,38 @@ impl Node {
         }
     }
 
-    fn comment(data: String) -> Self {
+    pub fn comment(data: &'a str) -> Self {
         Node {
             children: Vec::new(),
             node_type: NodeType::Comment(data),
         }
     }
+
+    pub fn pretty_print(&self) {
+        print_node(self, 0);
+    }
+}
+
+static PRINT_TAB: usize = 2;
+
+fn print_node(node: &Node, level: usize) {
+    match &node.node_type {
+        NodeType::Text(text) => leveled(format!("{}", text), level),
+        NodeType::Comment(comment) => leveled(format!("{}", comment), level),
+        NodeType::Element(elem) => {
+            print_elem(elem, &node.children, level);
+        }
+    }
+}
+
+fn print_elem(elem_data: &ElementData, children: &Vec<Node>, level: usize) {
+   leveled(format!("<{} {:?}>", elem_data.tag_name, elem_data.attributes), level);
+   for child in children.iter() {
+       print_node(child, level + 1);
+   }
+  leveled(format!("</{}>", elem_data.tag_name), level);
+}
+
+fn leveled(val: String, level: usize) {
+    println!("{:>indent$}", val, indent=PRINT_TAB*level + val.len());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::collections::HashMap;
+
+use dom::Node;
+
 mod dom;
 
 fn main() {
-    println!("Hello, world!");
+    let text1 = Node::text("text1");
+    let text2 = Node::text("text2");
+    let comment = Node::comment("Just commenting");
+    let div1 = Node::elem(
+        "div",
+        HashMap::from([("class", "data"), ("id", "main")]),
+        vec![comment, text1, text2],
+    );
+
+    div1.pretty_print();
 }
